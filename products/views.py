fa7915ca7752022eb0be20a4b2b6db937e78e3a5
Lee Edwards <edwardsla@live.co.uk>
2024-10-13 19:27:34 +0000

    FEAT: Adding filter box + functionality to retrieve selected category

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
     
@@ -11,9 +11,17 @@ def all_products(request):
     """
 
     products = Product.objects.all()
+    categories = Category.objects.all()
     query = None
+    select_category = None
 
     if request.GET:
+        if 'category' in request.GET:
+            category_filter = request.GET['category']
+            if category_filter:
+                select_category = category_filter.split(',')
+                products = products.filter(category__name__in=select_category)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -25,6 +33,8 @@ def all_products(request):
 
     context = {
         'products': products,
+        'categories': categories,
+        'select_category': select_category
     }
 
     return render(request, 'products/products.html', context)
