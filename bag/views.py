04063c8e3d8eef5d55fd7f45c70111361ad65471
Lee Edwards <edwardsla@live.co.uk>
2024-10-18 12:55:49 +0000

    FEAT: adding success messages for add to bag view

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
     
@@ -13,6 +16,7 @@ def add_to_bag(request, item_id):
     add a quantity of the  product to  bag
     """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -25,15 +29,20 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
+                messages.success(request, f'Added size {size.upper()}{product.name} quantity to {bag[item_id]["items_by_size"][size]}')
             else:
                 bag[item_id]['items_by_size'][size] = quantity
+                messages.success(request, f'Added size {size.upper()}{product.name} to bag')
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
+            messages.success(request, f'Added size {size.upper()}{product.name} to bag')
     else:
         if item_id in list(bag.keys()):
             bag[item_id] == quantity
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
