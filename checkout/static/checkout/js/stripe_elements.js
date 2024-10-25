3abd6211c9f078f13bcaf7eabbde1b1d56bff2e2
Lee Edwards <edwardsla@live.co.uk>
2024-10-25 13:59:08 +0000

    FEAT: Adding  Stripe elements for card payment handling with validation

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,38 @@
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+var card = elements.create('card', {style: style});
+
+var style = {
+    base: {
+        color: '#000',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#000'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+
+card.mount('#card-element');
+
+// handle realtime validation errors
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.errorS.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
