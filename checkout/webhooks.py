b6ced0fd7fdbe4cf96d48492dd4e7c6af118503b
Lee Edwards <edwardsla@live.co.uk>
2024-10-27 19:09:03 +0000

    CORRECTION: removing duplicate line

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -35,8 +35,6 @@ def webhook(request):
     except Exception as e:
         return HttpResponse(content=e, status=400)
 
-    handler = stripeWH_Handler(request)
-
     # Set up a webhook handler
     handler = StripeWH_Handler(request)
 
