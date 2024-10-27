351356afccb841ac0d8ffbb7016407f5a6863d52
Lee Edwards <edwardsla@live.co.uk>
2024-10-27 21:22:42 +0000

    FEAT: adding new fields to model and admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -11,14 +11,16 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total')
+                       'grand_total', 'original_bag', 
+                       'stripe_pid')
     
     fields = ('order_number', 'first_name', 
               'last_name', 'email', 'phone_number',
               'country', 'postcode', 'town_or_city',
               'house', 'street', 
               'county', 'date', 'delivery_cost', 
-              'order_total', 'grand_total')
+              'order_total', 'grand_total', 'original_bag', 
+              'stripe_pid')
 
     list_display = ('order_number', 'date', 
                     'first_name', 'last_name', 'delivery_cost', 
