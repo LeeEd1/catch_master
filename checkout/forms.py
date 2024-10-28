b042ba82abffbd374d231163924bd9d0642adbc3
Lee Edwards <edwardsla@live.co.uk>
2024-10-28 17:51:16 +0000

    FEAT: adding country dropdown along with migrations + styling

--- checkout/forms.py
+++ checkout/forms.py
@@ -21,7 +21,6 @@ class OrderForm(forms.ModelForm):
             'last_name': 'Last Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postcode',
             'town_or_city': 'Town or City',
             'house': 'House Number or Name',
@@ -31,10 +30,11 @@ class OrderForm(forms.ModelForm):
 
         self.fields['first_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
