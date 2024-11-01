82879a4ed3ae2bf2e92b38b93e4c414e893d8f0c
Lee Edwards <edwardsla@live.co.uk>
2024-11-01 12:01:51 +0000

    FIX: Correcting div to table and typo in views

--- profiles/views.py
+++ profiles/views.py
@@ -32,7 +32,7 @@ def profile(request):
     return render(request, template, context)
 
 def order_history(request, order_number):
-    order = get_object_or_404(order, order_number=order_number)
+    order = get_object_or_404(Order, order_number=order_number)
 
     messages.info(request, (
         f'This is a past order confirmation for order number {order_number}.'
