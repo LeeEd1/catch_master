f0249b26f0625b7d66b542c43a8fb3995c2736ad
Lee Edwards <edwardsla@live.co.uk>
2024-11-05 11:31:28 +0000

    FEAT: adding views for form and forms

--- catches/forms.py
+++ catches/forms.py
@@ -4,4 +4,5 @@ from .models import CatchEntry
 class CatchEntryForm(forms.ModelForm):
     class Meta:
         model = CatchEntry
-        fields = ('image', 'description')
+        fields = ('name', 'image_url', 
+                'image', 'description')
--- catches/views.py
+++ catches/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import CatchEntry
+from .forms import CatchEntryForm
 
 # Display all catch entries
 def catch_cam(request):
-    return render(request, 'catches/catch_cam.html')
+    entries = CatchEntry.objects.all()
+    return render(request, 'catches/catch_cam.html', {'entries': entries})
 
 def add_catch(request):
-    return render(request, 'catches/add_catch.html')
+    if request.method == 'POST':
+        form = CatchEntryForm(request.POST, request.FILES)
+        if form.is_valid():
+            catch_entry = form.save(commit=False)
+            catch_entry.user = request.user
+            catch_entry.save()
+            return redirect('catch_cam')
+    else:
+        form = CatchEntryForm()
+    
+    return render(request, 'catches/add_catch.html', {'form': form})
