14434b27e4ac2d4fd03457aaab41a2adeac3e3f7
Lee Edwards <edwardsla@live.co.uk>
2024-11-05 12:27:33 +0000

    FEAT: altering fields

--- catches/models.py
+++ catches/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 class CatchEntry(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255, blank=False, null=False, default="")
+    name = models.CharField(max_length=20, blank=False, null=False, default="")
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(blank=False, null=False, default="")
-    description = models.TextField(blank=False, null=False, default="")
+    description = models.TextField(max_length=254, blank=False, null=False, default="")
 
     def __str__(self):
         return self.name
