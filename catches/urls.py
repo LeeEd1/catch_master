b51e53f0f66b67a1d4b0585b8450a179226261f0
Lee Edwards <edwardsla@live.co.uk>
2024-11-05 10:46:57 +0000

    FEAT: Template for add catch and basic view to render + url

--- catches/urls.py
+++ catches/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.catch_cam, name='catch_cam'),
+    path('add/', views.add_catch, name='add_catch'),
 ]
--- catches/views.py
+++ catches/views.py
@@ -4,3 +4,6 @@ from .models import CatchEntry
 # Display all catch entries
 def catch_cam(request):
     return render(request, 'catches/catch_cam.html')
+
+def add_catch(request):
+    return render(request, 'catches/add_catch.html')
