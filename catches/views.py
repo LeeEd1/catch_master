852811ccf4dcfd0369fd22d86ab8cb4f148c7047
Lee Edwards <edwardsla@live.co.uk>
2024-11-05 12:29:19 +0000

    FEAT: add success and error messages to add_catch view

--- catches/views.py
+++ catches/views.py
@@ -11,14 +11,25 @@ def catch_cam(request):
 
 @login_required
 def add_catch(request):
+    """
+    Adds entry to Catch Cam
+    """
     if request.method == 'POST':
         form = CatchEntryForm(request.POST, request.FILES)
         if form.is_valid():
             catch_entry = form.save(commit=False)
             catch_entry.user = request.user
             catch_entry.save()
+            messages.success(request, 'Catch successfully added!')
             return redirect('catch_cam')
+        else:
+            messages.error(request, 'Failed to add catch! Please ensure the form is valid.')
     else:
         form = CatchEntryForm()
     
-    return render(request, 'catches/add_catch.html', {'form': form})
+    template = 'catches/add_catch.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
