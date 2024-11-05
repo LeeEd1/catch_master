2f8d7b3bad8cb8613cb685c2d04de52649fa3b37
Lee Edwards <edwardsla@live.co.uk>
2024-11-05 12:52:25 +0000

    FEAT: adding button for edit + url

--- catches/urls.py
+++ catches/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.catch_cam, name='catch_cam'),
     path('add/', views.add_catch, name='add_catch'),
+    path('edit/<int:catch_id>/', views.edit_catch, name='edit_catch'),
 ]
