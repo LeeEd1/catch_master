08558cb1f69e9fc9fe91853c34aff01c3f8a2e56
Lee Edwards <edwardsla@live.co.uk>
2024-11-06 11:14:19 +0000

    FEAT: making both image fields optional after realising the url becomes redundant

--- catches/models.py
+++ catches/models.py
@@ -5,7 +5,7 @@ class CatchEntry(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=20, blank=False, null=False, default="")
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(blank=False, null=False, default="")
+    image = models.ImageField(blank=True, null=True)
     description = models.TextField(max_length=254, blank=False, null=False, default="")
 
     def __str__(self):
