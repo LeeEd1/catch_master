a3aaa12e84fa137da739ac372dcc28d1f19259bb
Lee Edwards <edwardsla@live.co.uk>
2024-12-04 10:41:57 +0000

    FEAT: custom code for admin panel

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at',)
+    fields = ('name', 'email', 'phone_number', 'message', 'created_at')
+
+    list_display = ('name', 'email', 'phone_number', 'created_at')
+
+    ordering = ('-created_at',)
