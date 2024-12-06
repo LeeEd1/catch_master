45c1dde61e6f28e4c7d5402b058860d17df16f86
Lee Edwards <edwardsla@live.co.uk>
2024-12-06 05:56:21 +0000

    CHORE: adding docstrings to views

--- contact/views.py
+++ contact/views.py
@@ -3,12 +3,15 @@ from django.contrib import messages
 from .forms import ContactForm
 
 def contact(request):
+    """
+    Handle the display and submission of the contact form.
+    """
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Your message has been sent successfully!')
-            return redirect('contact')
+            return redirect('thank_you')
         else:
             messages.error(request, 'There was an error with your form, Please try again.')
     else:
@@ -18,4 +21,7 @@ def contact(request):
 
 
 def thank_you(request):
+    """
+    Render the thank-you page after successful form submission.
+    """
     return render(request, 'contact/thankyou.html')
